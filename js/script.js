0f23b75423b5d59828eff689bd69c3867717d854
stobin <stobin@stobin.net>
2019-02-25 15:23:42 +0100

    Add first variable in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 //printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+
+/*printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');*/
+
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
