b342c0926133981848d8f8a17707cac7f3c7fd00
stobin <stobin@stobin.net>
2019-02-25 17:35:18 +0100

    Add first condition if else

--- js/script.js
+++ js/script.js
@@ -1,23 +1,10 @@
-//printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-
-/*printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');*/
-
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-var playerMove;
-playerMove = "papier";
-printMessage('Zagrałam ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywam!');
-
-/* an integer range 11-19
-var randomNumber;
-randomNumber = Math.floor((Math.random() * 9) + 11);
-printMessage('Wylosowana liczba to: ' + randomNumber); */
-
-// an integer range 1-3
-var randomNumber;
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
 
