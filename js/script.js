519f186e68dd8c63bef76ab4f7ecb22f71cb7a0c
stobin <stobin@stobin.net>
2019-02-25 18:40:44 +0100

    Add basic interaction with user

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ if (randomNumber == '1') {
 
 printMessage('Mój ruch: ' + computerMove);
 
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else {
+    printMessage('Twój ruch jest błędny! Użyto domyślnej wartości');
+    playerMove = 'kamień';
+}
+
+printMessage('Twój ruch: ' + playerMove);
+
