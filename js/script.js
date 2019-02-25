9d6f006404957f04db5bf811b5524107e5c21171
stobin <stobin@stobin.net>
2019-02-25 15:17:44 +0100

    Add first text concatenation in JS

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
